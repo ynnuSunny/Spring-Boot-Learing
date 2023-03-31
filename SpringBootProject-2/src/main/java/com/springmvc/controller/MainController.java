24b0a9fbb2976d0fff53cdf48252be46d7308f5a
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-01 04:20:21 +0600

    Backed to front end user Model Interface Refreance and view useing Thymrleaf

--- SpringBootProject-2/src/main/java/com/springmvc/controller/MainController.java
+++ SpringBootProject-2/src/main/java/com/springmvc/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class MainController {
@@ -13,8 +16,12 @@ public class MainController {
     }
 	
 	@RequestMapping("addProgrammer")
-    public String programmer(@RequestParam int pId,@RequestParam String pName,@RequestParam String pLang) {
-        System.out.println(+pId+" "+pName+" "+pLang);
+    public String programmer(@RequestParam int pId,@RequestParam String pName,@RequestParam String pLang,Model model) {
+       // System.out.println(+pId+" "+pName+" "+pLang);
+		model.addAttribute("pId",pId);
+		model.addAttribute("pName",pName);
+		model.addAttribute("pLang",pLang);
+		
 		return "ProgrammerInfo.html";
     }
 }
