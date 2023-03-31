bd53bc07e495448519958f5e897fe64b10fa65b2
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-01 04:12:36 +0600

    take input from form using RequestParm Annotation

--- SpringBootProject-2/src/main/java/com/springmvc/controller/MainController.java
+++ SpringBootProject-2/src/main/java/com/springmvc/controller/MainController.java
@@ -2,6 +2,7 @@ package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,5 +10,11 @@ public class MainController {
 	@RequestMapping("index")
     public String homePage() {
     	return "HomePage.html";
+    }
+	
+	@RequestMapping("addProgrammer")
+    public String programmer(@RequestParam int pId,@RequestParam String pName,@RequestParam String pLang) {
+        System.out.println(+pId+" "+pName+" "+pLang);
+		return "ProgrammerInfo.html";
     }
 }
