832b8be6a19ce8cf1cac46b5475078fa37b803dc
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-01 03:36:22 +0600

    Start Spring MVC simple project

---
+++ SpringBootProject-2/src/main/java/com/springmvc/SpringBootProject2Application.java
@@ -0,0 +1,13 @@
+package com.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject2Application.class, args);
+	}
+
+}
---
+++ SpringBootProject-2/src/test/java/com/springmvc/SpringBootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
