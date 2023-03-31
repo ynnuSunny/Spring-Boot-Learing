45dda07fd6ab269f187df1a4f174988bca75ba65
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-01 03:51:59 +0600

    Recive User Request by Controller and send html page as response

---
+++ SpringBootProject-2/src/main/java/com/springmvc/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("index")
+    public String homePage() {
+    	return "HomePage.html";
+    }
+}
