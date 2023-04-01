73d45ae5d20f4678e15aed0a0c162e25c9f9a463
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-01 23:16:52 +0600

    Start Project

---
+++ Online-Exam-Taker/src/main/java/com/onlineexam/OnlineExamTakerApplication.java
@@ -0,0 +1,13 @@
+package com.onlineexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamTakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamTakerApplication.class, args);
+	}
+
+}
---
+++ Online-Exam-Taker/src/test/java/com/onlineexam/OnlineExamTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamTakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
