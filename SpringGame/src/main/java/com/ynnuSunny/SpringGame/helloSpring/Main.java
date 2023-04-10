d855a1c97b735a8e2fb39c652b9009bf8db5a3cf
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-10 21:07:52 +0600

    Spring Bean and AutoWirein Using ApplicationContext

--- SpringGame/src/main/java/com/ynnuSunny/SpringGame/helloSpring/Main.java
+++ SpringGame/src/main/java/com/ynnuSunny/SpringGame/helloSpring/Main.java
@@ -1,5 +1,7 @@
 package com.ynnuSunny.SpringGame.helloSpring;
 
+import java.util.Arrays;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,7 +15,15 @@ public class Main {
 		  //2: Configure the things that we want Spring to manage
 		// by @Configuration 
 		  
+		  
+		  //3: Retrieving Beans Managed by Spring
 		  System.out.println(context.getBean("name"));
+		  System.out.println(context.getBean("personBuildUseBean"));
+		  System.out.println(context.getBean(Person.class));
+		  System.out.println(context.getBean("person3"));
+		  
+		  //System.out.println
+		  Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
 	}
 
--- SpringGame/src/main/java/com/ynnuSunny/SpringGame/helloSpring/SpringContextOrBean.java
+++ SpringGame/src/main/java/com/ynnuSunny/SpringGame/helloSpring/SpringContextOrBean.java
@@ -1,7 +1,13 @@
 package com.ynnuSunny.SpringGame.helloSpring;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+record Person(String name,int age,Address address) { };
+record Address(String firstLine,String city) {};
+
 
 @Configuration
 public class SpringContextOrBean {
@@ -10,5 +16,53 @@ public class SpringContextOrBean {
 	public String name() {
 		return "Sunny";
 	}
+    
+    @Bean
+    public int age() {
+    	return 22;
+    }
+    
+    @Bean
+    @Primary
+    public Person person(Address address) {
+    	return new Person(name(),age(),address);
+    }
+    
+    @Bean
+    public Person personBuildCostom() {
+    	return new Person("Rahul",15,new Address("Kankir Hat","Noakhali"));
+    }
+    
+    @Bean
+    public Person personBuildUseBean() {
+    	return new Person(name(),age(),address());
+    }
+    
+    @Bean
+    public Person person3(@Qualifier("addressQualifier") Address address) {
+    	return new Person(name(),age(),address);
+    }
+    
+    @Bean
+    public Address address() {
+         return new Address("Baker Street","London");
+    }
+    
+    @Bean(name = "address1")
+    @Primary
+    public Address customAddress1() {
+         return new Address("Mudga","Dhaka");
+    }
+    
+    @Bean
+    @Qualifier("addressQualifier")
+    public Address addressWithQualifier() {
+         return new Address("Sobujbag","Khulna");
+    }
+    
+    
+    
+    
+    
 	
 }
