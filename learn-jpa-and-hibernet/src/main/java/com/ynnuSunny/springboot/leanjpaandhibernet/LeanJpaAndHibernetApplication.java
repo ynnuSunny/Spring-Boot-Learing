46dbf63d7a91fc76a0939b26af18f4f385cb1c5b
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-18 14:16:17 +0600

    Start new project

---
+++ learn-jpa-and-hibernet/src/main/java/com/ynnuSunny/springboot/leanjpaandhibernet/LeanJpaAndHibernetApplication.java
@@ -0,0 +1,13 @@
+package com.ynnuSunny.springboot.leanjpaandhibernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanJpaAndHibernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanJpaAndHibernetApplication.class, args);
+	}
+
+}
---
+++ learn-jpa-and-hibernet/src/test/java/com/ynnuSunny/springboot/leanjpaandhibernet/LeanJpaAndHibernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynnuSunny.springboot.leanjpaandhibernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanJpaAndHibernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
