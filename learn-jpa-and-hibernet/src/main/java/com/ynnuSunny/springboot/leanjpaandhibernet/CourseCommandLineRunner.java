1d182eaaeca35e1610a286cfbdefee50b6d01e9a
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-04-19 13:33:58 +0600

    Spring DataJpa Basic Querys

--- learn-jpa-and-hibernet/src/main/java/com/ynnuSunny/springboot/leanjpaandhibernet/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernet/src/main/java/com/ynnuSunny/springboot/leanjpaandhibernet/CourseCommandLineRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.ynnuSunny.springboot.leanjpaandhibernet.course.Course;
+import com.ynnuSunny.springboot.leanjpaandhibernet.datajpa.DataJpaRepository;
 import com.ynnuSunny.springboot.leanjpaandhibernet.jdbc.JdbcRepository;
 import com.ynnuSunny.springboot.leanjpaandhibernet.jpa.JpaRepository;
 
@@ -14,23 +15,43 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 //	@Autowired
 //	private JdbcRepository repository;
 	
-	@Autowired
-	private JpaRepository repository;
+//	@Autowired
+//	private JpaRepository repository;
+//	
+//	
+//	@Override
+//	public void run(String... args) throws Exception {
+//		  
+//		   repository.insert(new Course(1,"Java","Sunny Bro"));
+//		   repository.insert(new Course(2,"C","Sunny Vai"));
+//		   repository.insert(new Course(3,"C++","Sunny Dada"));
+//		   repository.insert(new Course(4,"Python","Sunny"));
+//		   
+//		   repository.deleteByid(2);
+//		   
+//		   System.out.println(repository.findByid(1));
+//		   System.out.println(repository.findByid(3));
+//		   System.out.println(repository.findByid(4));
+//		
+//	}
 	
+	@Autowired
+	DataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		  
-		   repository.insert(new Course(1,"Java","Sunny Bro"));
-		   repository.insert(new Course(2,"C","Sunny Vai"));
-		   repository.insert(new Course(3,"C++","Sunny Dada"));
-		   repository.insert(new Course(4,"Python","Sunny"));
+		   repository.save(new Course(1,"Java","Sunny Bro"));
+		   repository.save(new Course(2,"C","Sunny Vai"));
+		   repository.save(new Course(3,"C++","Sunny Dada"));
+		   repository.save(new Course(4,"Python","Sunny"));
 		   
-		   repository.deleteByid(2);
+		   repository.deleteById(3l);
 		   
-		   System.out.println(repository.findByid(1));
-		   System.out.println(repository.findByid(3));
-		   System.out.println(repository.findByid(4));
+		   System.out.println(repository.findById(1l));
+		   System.out.println(repository.findById(2l));
+		   System.out.println(repository.findByAuthor("Sunny"));
 		
 	}
+	
+	
 }
--- learn-jpa-and-hibernet/src/main/java/com/ynnuSunny/springboot/leanjpaandhibernet/datajpa/DataJpaRepository.java
+++ learn-jpa-and-hibernet/src/main/java/com/ynnuSunny/springboot/leanjpaandhibernet/datajpa/DataJpaRepository.java
@@ -1,6 +1,7 @@
 package com.ynnuSunny.springboot.leanjpaandhibernet.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import com.ynnuSunny.springboot.leanjpaandhibernet.course.Course;
 
 
-public class DataJpaRepository {
-	 
+public interface DataJpaRepository extends JpaRepository<Course,Long> {
+	 public Course findByAuthor(String author);
 }
